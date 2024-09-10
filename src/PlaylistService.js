9c4810aa5dfd576e8876c2a133e6898d345dd9c6
Fauzan Rizky <frizky.erlang@gmail.com>
2024-09-11 00:07:43 +0700

    create playlist service

---
+++ src/PlaylistService.js
@@ -0,0 +1,34 @@
+const { Pool } = require('pg');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getPlaylistById(playlistId) {
+    const query = {
+      text: `SELECT playlists.id, playlists.name
+      FROM playlists
+      WHERE playlists.id = $1`,
+      values: [playlistId],
+    };
+    const result = await this._pool.query(query);
+
+    return result.rows[0];
+  }
+
+  async getPlaylistSongById(playlistId) {
+    const query = {
+      text: `SELECT songs.id, songs.title, songs.performer
+      FROM playlist_songs
+      LEFT JOIN songs on playlist_songs.song_id = songs.id
+      WHERE playlist_id = $1`,
+      values: [playlistId],
+    };
+    const result = await this._pool.query(query);
+
+    return result.rows;
+  }
+}
+
+module.exports = PlaylistsService;
